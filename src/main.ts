908353f041c3e2346463956126d1709a8885ac8b
uguisu-an <arcana278@gmail.com>
2019-06-01 09:26:11 +0900

    小さいフォームを作ってみた

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./main.scss";
 
 Vue.config.productionTip = false;
 
---
+++ src/views/TestPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container border py-2 rounded" style="width: 15rem;">
+    <form class="form-sm">
+      <div class="form-group">
+        <input type="text" class="form-control" placeholder="氏名" />
+      </div>
+      <div class="form-group">
+        <input type="text" class="form-control" />
+      </div>
+      <div class="form-group">
+        <input type="text" class="form-control" />
+      </div>
+      <div class="d-flex flex-row-reverse">
+        <button class="btn btn-primary ml-2">Send</button>
+        <button class="btn btn-secondary ml-2">Cancel</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
