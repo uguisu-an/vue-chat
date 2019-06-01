83848fe240f4662e60525f937f3a2629b38f8a65
uguisu-an <arcana278@gmail.com>
2019-06-01 09:30:16 +0900

    form-btn-groupにボタンの設定をまとめた

--- src/views/TestPage.vue
+++ src/views/TestPage.vue
@@ -10,9 +10,9 @@
       <div class="form-group">
         <input type="text" class="form-control" />
       </div>
-      <div class="d-flex flex-row-reverse">
-        <button class="btn btn-primary ml-2">Send</button>
-        <button class="btn btn-secondary ml-2">Cancel</button>
+      <div class="form-btn-group">
+        <button class="btn btn-primary">Send</button>
+        <button class="btn btn-secondary">Cancel</button>
       </div>
     </form>
   </div>
