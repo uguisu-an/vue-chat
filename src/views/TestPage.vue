1b93f045f6dfc43f64e7d28b6fc73c3550f6759f
uguisu-an <arcana278@gmail.com>
2019-06-02 08:07:47 +0900

    モーダル出した

--- src/views/TestPage.vue
+++ src/views/TestPage.vue
@@ -1,26 +1,46 @@
 <template>
-  <div class="container border py-2 rounded" style="width: 15rem;">
-    <form class="form-sm">
-      <div class="form-group">
-        <input type="text" class="form-control" placeholder="氏名" />
+  <div class="container" style="width: 10rem;">
+    <div class="task-group" v-for="tg in taskGroups" :key="tg.id">
+      <a href="#" @click="currenctTaskGroup = tg">
+        {{tg.name}}
+      </a>
+      <div class="task-group__modal p-2 border bg-light" v-if="currenctTaskGroup === tg">
+        <h5>{{ tg.name }}</h5>
+        <button @click="currenctTaskGroup = null" class="btn btn-sm btn-primary">OK</button>
       </div>
-      <div class="form-group">
-        <input type="text" class="form-control" />
-      </div>
-      <div class="form-group">
-        <textarea class="form-control" rows="3"></textarea>
-      </div>
-      <div class="form-btn-group">
-        <button class="btn btn-primary">Send</button>
-        <button class="btn btn-secondary">Cancel</button>
-      </div>
-    </form>
+    </div>
   </div>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class TestPage extends Vue {
+  currenctTaskGroup = null;
+  taskGroups = [
+    { id: 1, name: "Hello world"},
+    { id: 2, name: "Hi!"},
+    { id: 3, name: "Bye"},
+    { id: 4, name: "Task 4"},
+  ]
+
+
+  test1 = false;
+}
 </script>
 
-<style>
+<style lang="scss" scoped>
+.task-group {
+  position: relative;
+}
+
+.task-group__modal {
+  position: absolute;
+  left: 0;
+  top: 1.5rem;
+  width: 16rem;
+  height: 10rem;
+  z-index: 100;
+}
 </style>
